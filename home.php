0fdb432706429a1e2396deb0c0dd455334cb7ac9
StephenSanjaya <stephensanjaya5@gmail.com>
2024-02-14 15:58:57 +0700

    add about us page

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- <?php include './template/header.php'; ?> -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food</title>
+    <link rel="stylesheet" href="./asset/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+</head>
+<body>
+    <?php include './template/navbar.php' ?>
+
+    <section id="main-container">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Enjoy Our Food</h1>
+                <div class="btn-menu">
+                    <button class="btn btn-outline-dark">Explore Our Menu <span class="material-symbols-outlined">chevron_right</span></button>
+                </div>
+            </div>
+        </div>
+  </section>
+</body>
+</html>
---
+++ template/navbar.php
@@ -0,0 +1,30 @@
+<nav class="stroke navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+        <a class="navbar-brand text-light fs-1" href="#">Food</a>
+        <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active text-light fs-5" aria-current="page" href="./home.php">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-light fs-5" href="#">Product</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-light fs-5" href="#">Gallery</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-light fs-5" href="./about_us.php">About Us</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-light fs-5" href="#">Contact Us</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-light fs-5" href="#">Login/Register</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
