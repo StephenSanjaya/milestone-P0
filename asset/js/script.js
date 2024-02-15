0403099df9da9c1bc8b11dba9f1d1e147fcc4204
StephenSanjaya <stephensanjaya5@gmail.com>
2024-02-15 12:00:29 +0700

    add menu page + js function

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,20 +1,50 @@
-function renderImage() {
-    var selected = document.getElementById("selectOption");
-    var imgUrl = "";
-    var title = "";
-    if (selected.value == 1) {
-        imgUrl = "./asset/image/food1.jpg";
-        title = "le cous aksdnla";
-    } else if (selected.value == 2) {
-        imgUrl = "./asset/image/food2.jpg";
-        title = "le cous mvcmvncmv";
-    } else if (selected.value == 3) {
-        imgUrl = "./asset/image/food3.jpg";
-        title = "le cous wqweqweqw";
-    }
-
-    console.log(imgUrl);
+var price="";
+function initMenu() {
+    var imgUrl = "./asset/image/food1.jpg";
+    var name = "le cous aksdnla";
+    var person = 1;
+    price = 300000;
+    var totalPrice = person * price;
 
     document.getElementById("idImg").src = imgUrl;
-    document.getElementById("menu-title").innerHTML = title;
+    document.getElementById("menu-name").innerHTML = name;
+    document.getElementById("tbl-menu-name").innerHTML = name;
+
+    document.getElementById("tbl-menu-price").innerHTML = totalPrice;
+    document.getElementById("tbl-menu-person").innerHTML = person;
+}
+
+function renderMenu() {
+    var selected = document.getElementById("selectOptionMenu");
+    var imgUrl = "";
+    var name = "";
+    price = "";
+    if (selected.value == 1) {
+        imgUrl = "./asset/image/food1.jpg";
+        name = "le cous aksdnla";
+        price = 300000;
+    } else if (selected.value == 2) {
+        imgUrl = "./asset/image/food2.jpg";
+        name = "le cous mvcmvncmv";
+        price = 500000;
+    } else if (selected.value == 3) {
+        imgUrl = "./asset/image/food3.jpg";
+        name = "le cous wqweqweqw";
+        price = 700000;
+    }
+
+    document.getElementById("idImg").src = imgUrl;
+    document.getElementById("menu-name").innerHTML = name;
+    document.getElementById("tbl-menu-name").innerHTML = name;
+
+    renderPerson();
+}
+
+function renderPerson() {
+    var selected = document.getElementById("selectOptionPerson");
+    var person = selected.value;
+    var totalPrice = person * price;
+
+    document.getElementById("tbl-menu-price").innerHTML = totalPrice;
+    document.getElementById("tbl-menu-person").innerHTML = person;
 }
--- menu.php
+++ menu.php
@@ -3,35 +3,54 @@
 <head>
     <?php include './template/header.php'; ?>
 </head>
-<body>
+<body onload="initMenu()">
     <?php include './template/navbar.php' ?>
 
     <section id="menu-section">
         <div class="menu-image">
-            <img id="idImg" src="./asset/image/food1.jpg" class="img-fluid" alt="food1-img">
-            <h1 id="menu-title">
-                le cous aksdnla
-            </h1>
+            <img id="idImg" src="" class="img-fluid" alt="food1-img">
+            <h1 id="menu-name"></h1>
         </div>
         <div class="menu-selection">
-            <select id="selectOption" class="form-select" aria-label="Default select example" onchange="renderImage()">
-                <option selected="selected" value="1">menu 1</option>
-                <option value="2">menu 2</option>
-                <option value="3">menu 3</option>
+            <select id="selectOptionMenu" class="form-select" aria-label="Default select example" onchange="renderMenu()">
+                <option selected value="1">Menu 1</option>
+                <option value="2">Menu 2</option>
+                <option value="3">Menu 3</option>
             </select>
-            <select class="form-select" aria-label="Default select example">
-                <option value="1">1 person</option>
-                <option value="2">2 person</option>
-                <option value="3">3 person</option>
-                <option value="4">4 person</option>
-                <option value="5">5 person</option>
-                <option value="6">6 person</option>
-                <option value="7">7 person</option>
+            <select id="selectOptionPerson" class="form-select" aria-label="Default select example" onchange="renderPerson()">
+                <option selected value="1">1 Person</option>
+                <option value="2">2 Person</option>
+                <option value="3">3 Person</option>
+                <option value="4">4 Person</option>
+                <option value="5">5 Person</option>
+                <option value="6">6 Person</option>
+                <option value="7">7 Person</option>
             </select>
+        </div>
+        <div class="calc-price">
+        <table class="table borderless">
+            <thead>
+                <tr>
+                    <th scope="col">Menu</th>
+                    <th scope="col">Total Person</th>
+                    <th scope="col">Total Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td id="tbl-menu-name"></td>
+                    <td id="tbl-menu-person"></td>
+                    <td id="tbl-menu-price"></td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        <div id="btn-book">
+            <button type="button" class="btn btn-success w-100">Book</button>
         </div>
     </section>
 
-    <!-- <?php include './template/footer.php' ?> -->
+    <?php include './template/footer.php' ?>
 
     <script src="./asset/js/script.js"></script>
 </body>
